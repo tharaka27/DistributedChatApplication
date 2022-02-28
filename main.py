dc2fcf0acc7a9ba5e9e02a6be325f1e68abd207f
Tharaka Ratnayake <tharakasachintha27@gmail.com>
2022-02-28 22:25:12 +0530

    test for localroominfo and remoteroominfo

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import time
 import threading
 import random
-from testcases import test_threadpool, test_chatroominfo , test_configuration
+from testcases import test_threadpool, test_chatroominfo , test_configuration, test_localroominfo, test_remoteroominfo
 
 def main():
     #test_threadpool.test()
     #test_chatroominfo.test()
-    test_configuration.test()
+    #test_configuration.test()
+    #test_localroominfo.test()
+    test_remoteroominfo.test()
 
 
 if __name__ == "__main__":
