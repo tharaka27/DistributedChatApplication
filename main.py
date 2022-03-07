de1eb3c89d935f82c3cd077c8808699c153157f6
Tharaka Ratnayake <tharakasachintha27@gmail.com>
2022-03-07 14:35:21 +0530

    migrating to flask

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import time
 import threading
 import random
-from testcases import test_threadpool, test_chatroominfo , test_configuration, test_localroominfo, test_remoteroominfo, test_fileReader
+#from testcases import test_threadpool, test_chatroominfo , test_configuration, test_localroominfo, test_remoteroominfo, test_fileReader
+from flask import Flask, jsonify, request
 
+# creating a Flask app
+app = Flask(__name__)
+
+# on the terminal type: curl http://127.0.0.1:5000/
+# returns the data that we send when we use POST.
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    if(request.method == 'POST'):
+        json_data = request.json
+        data = "hello world by post" + str(json_data)
+        return jsonify({'data': data})
+
+
+# driver function
+if __name__ == '__main__':
+	app.run(debug = True)
+
+
+'''
 def main():
     #test_threadpool.test()
     #test_chatroominfo.test()
@@ -14,3 +34,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+'''
